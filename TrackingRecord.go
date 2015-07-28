b6a33ff49f736de60d2d6901ef22622d910f4776
medhagupta <medhagupta1992@gmail.com>
2015-07-28 19:02:40 +0530

    Added merge function in TrackingRecord

--- TrackingRecord.go
+++ TrackingRecord.go
@@ -13,3 +13,22 @@ func copyTrackingRecord(record TrackingRecord) TrackingRecord{
 	return copyRecord
 }
 
+func merge(trackingRecords []TrackingRecord )[]TrackingRecord{
+	var mergedtrackingRecords[]TrackingRecord
+	var outputPointer int = 0
+	mergedtrackingRecords = append(mergedtrackingRecords,trackingRecords[0]);
+	for i := 1; i < len(trackingRecords); i++ {
+      if(trackingRecords[i].lo-mergedtrackingRecords[outputPointer].hi==1) &&
+	  (trackingRecords[i].statusCode == mergedtrackingRecords[outputPointer].statusCode) &&
+	  (trackingRecords[i].transferCode == mergedtrackingRecords[outputPointer].transferCode){
+	  	  
+		//t  := TrackingRecord{trackingRecords[i].statusCode,trackingRecords[i].transferCode,Range{mergedtrackingRecords[outputPointer].lo,trackingRecords[i].hi}}
+			mergedtrackingRecords[outputPointer].hi = trackingRecords[i].hi
+			} else{
+			t  := TrackingRecord{trackingRecords[i].statusCode,trackingRecords[i].transferCode,Range{trackingRecords[i].lo,trackingRecords[i].hi}}
+			mergedtrackingRecords = append(mergedtrackingRecords,t)
+			outputPointer+=1
+		}
+    }
+	return mergedtrackingRecords
+	}
